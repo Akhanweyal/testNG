66f8c41f3f91e475bd130c8f7cf6ddbd893402c0
Ajwkh <Ajwkh>
2025-03-17 16:14:44 -0400

    selenium gird

--- src/test/java/com/testng/DynamicElementUsingXpath.java
+++ src/test/java/com/testng/DynamicElementUsingXpath.java
@@ -1,26 +1,28 @@
 package com.testng;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.net.URL; // Add this import statement
 
 public class DynamicElementUsingXpath {
 
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
-        // Set up EdgeDriver using WebDriverManager
-        WebDriverManager.edgedriver().setup();
-        driver = new EdgeDriver();
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("edge");
+
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"), capabilities);
+        driver.get("https://www.google.com");
     }
 
     @Test
     public void testGoogleSearch() {
-        driver.get("https://www.google.com");
         System.out.println("Page Title: " + driver.getTitle());
         assert driver.getTitle().contains("Google");
     }
